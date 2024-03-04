b72186116733eb5983e46444a02204df270f5a3c
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-03-04 11:11:03 +0400

    added some urls and view

--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Tag(models.Model):
-    name = models.CharField(max_length =True , unique =True)
+    name = models.CharField(max_length =200 , unique =True)
     
     def __str__(self):
         return self.name
     
 class Post(models.Model):
-    title= models.CharField(max_length =200)
+    title= models.CharField(max_length = 200)
     content= models.TextField()
     author = models.ForeignKey(User, on_delete =models.CASCADE , related_name = 'posts')
     tags = models.ManyToManyField(Tag , related_name='posts')
@@ -18,18 +18,23 @@ class Post(models.Model):
     
     class Meta:
         indexes = [ 
-            models.Index(fields='created_at')
-       ]
+            models.Index(fields=['created_at'])
+    ]
     
     def __str__(self):
         return self.title
     
-class comments(models.Model):
+class Comment(models.Model):
     post =models.ForeignKey(Post, on_delete =models.CASCADE, related_name = 'comments')
     author =models.ForeignKey(User, on_delete =models.CASCADE, related_name = 'comments')
     content= models.TextField()
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        indexes = [ 
+            models.Index(fields=['created_at'])
+    ]
     
     def __str__(self):
         return f"comment by {self.author} on {self.post}"
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import generics
+from .models import Post, Comment, Tag
+from .serializers import PostSerializer , CommentSerializer , TagSerializer
 
-# Create your views here.
+
+class PostList(generics.ListCreateAPIView):
+    queryset= Post.objects.all()
+    serializer_class = PostSerializer
+    
+
